8604c0f8d3276db2f25c17726cad54fcf650a035
Simon Hacks <shacks@kth.se>
2019-12-31 15:02:42 +0100

    Attack Builder added

--- src/main/java/se/kth/mal/transformator/builder/AttackBuilder.java
+++ src/main/java/se/kth/mal/transformator/builder/AttackBuilder.java
@@ -2,6 +2,7 @@ package se.kth.mal.transformator.builder;
 
 import se.kth.mal.transformator.model.Asset;
 import se.kth.mal.transformator.model.Attack;
+import se.kth.mal.transformator.model.Probability;
 
 import java.util.HashMap;
 
@@ -14,6 +15,18 @@ public class AttackBuilder {
 
     public Attack getAttack(Asset parentAsset, String name, String probability, Asset targetAsset, String targetAttack) {
         Attack newAttack = new Attack();
+
+        newAttack.setAsset(parentAsset);
+        newAttack.setName(name);
+        newAttack.setProbability(new Probability(probability));
+        newAttack.getRelatedAttacks().add(getAttack(targetAsset, targetAttack));
+
+        if (attacks.containsKey(newAttack.getIdentifier())) {
+            attacks.get(newAttack.getIdentifier()).merge(newAttack);
+            newAttack = attacks.get(newAttack.getIdentifier());
+        } else {
+            attacks.put(newAttack.getIdentifier(), newAttack);
+        }
 
         return newAttack;
     }
